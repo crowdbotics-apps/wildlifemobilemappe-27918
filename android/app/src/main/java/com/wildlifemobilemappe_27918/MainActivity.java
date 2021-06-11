bba04f9e89c7f85537f2760ddd1fa3062ab8429d
Crowdbotics <team@crowdbotics.com>
2021-06-11 13:07:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/wildlifemobilemappe_27918/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wildlifemobilemappe_27918;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "wildlifemobilemappe_27918";
+  }
+}
